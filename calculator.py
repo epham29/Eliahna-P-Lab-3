c1c1c1c750cab90170b5cadf6e65cb51d27583ed
Elliahna Pham <epham29@setoncatholic.org>
2026-09-23 18:59:01 +0000

    commit 1

---
+++ calculator.py
@@ -0,0 +1,20 @@
+# Function that adds two numbers
+def add(x,y):
+    print(x+y)
+
+# Function that subtracts two numbers
+def subtract(x,y):
+    print(x-y)
+
+# Function that multiplies two numbers
+def multiply(x,y):
+    print(x*y)
+
+# Function that divides two numbers
+def divide(x,y):
+    print(x/y)
+
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: ")
+
+print("Welcome")
---
+++ feed.py
@@ -0,0 +1,10 @@
+def feed_the_dog():
+    print("nom nom nom")
+def feed_the_cat():
+    print("hisssssss")
+def feed_the_fish():
+    print("glug glug glug")
+
+feed_the_dog()
+feed_the_cat()
+feed_the_fish()
